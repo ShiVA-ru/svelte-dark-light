73428263ab3affe690f7cb0b87f5f19ee736a642
Shinyaev Vladimir <secman@bk.ru>
2025-05-07 14:40:31 +0500

    implement dark-light switching

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="page container mx-auto">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+
+	<button
+		class="rounded-2xl border-2 p-2 transition-all hover:rounded-4xl"
+		onclick={window?.toggleTheme}
+		>change theme
+	</button>
+</div>
+
+<style>
+	.page {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 20px;
+
+		color: var(--primary-text-color);
+	}
+	button {
+		color: var(--button-text-color);
+		background-color: var(--button-background-color);
+
+		cursor: pointer;
+	}
+</style>
